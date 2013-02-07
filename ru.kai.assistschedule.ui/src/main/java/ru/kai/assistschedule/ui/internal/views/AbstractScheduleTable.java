5c29dab0b02dc7cefae9765bf46676f89257201e
romanrich89 <romanrich89@gmail.com>
2013-02-07 18:55:21 +0000

    При клике по заголовку колонки вылезает поле с выбором фильтрации

--- ru.kai.assistschedule.ui/src/main/java/ru/kai/assistschedule/ui/internal/views/AbstractScheduleTable.java
+++ ru.kai.assistschedule.ui/src/main/java/ru/kai/assistschedule/ui/internal/views/AbstractScheduleTable.java
@@ -1,6 +1,7 @@
 package ru.kai.assistschedule.ui.internal.views;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnViewerEditor;
@@ -14,10 +15,18 @@ import org.eclipse.nebula.jface.gridviewer.GridTableViewer;
 import org.eclipse.nebula.jface.gridviewer.GridViewerEditor;
 import org.eclipse.nebula.widgets.grid.GridColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.ShellListener;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 import ru.kai.assistschedule.core.IScheduleTable;
@@ -26,140 +35,161 @@ import ru.kai.assistschedule.ui.internal.widgets.ExcelFilter;
 
 public abstract class AbstractScheduleTable implements IScheduleTable {
 
-    private GridTableViewer v;
+	private GridTableViewer v;
 
-    private Composite composite;
+	private Composite composite;
 
-    public AbstractScheduleTable(Composite parent) {
-	parent.setLayout(new FillLayout());
-	composite = new Composite(parent, SWT.NONE);
-	composite.setLayout(new FillLayout());
-	v = new GridTableViewer(composite, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL);
-	v.setLabelProvider(getLabelProvider());
-	v.setContentProvider(getContentProvider());
-	v.getGrid().setCellSelectionEnabled(true);
+	public AbstractScheduleTable(Composite parent) {
+		parent.setLayout(new FillLayout());
+		composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new FillLayout());
+		v = new GridTableViewer(composite, SWT.BORDER | SWT.V_SCROLL
+				| SWT.H_SCROLL);
+		v.setLabelProvider(getLabelProvider());
+		v.setContentProvider(getContentProvider());
+		v.getGrid().setCellSelectionEnabled(true);
+
+		v.setCellEditors(new CellEditor[] { new TextCellEditor(v.getGrid()),
+				new TextCellEditor(v.getGrid()) });
+		v.setCellModifier(new ICellModifier() {
+
+			public boolean canModify(Object element, String property) {
+				return true;
+			}
+
+			public Object getValue(Object element, String property) {
+				return "Column " + property + " => " + element.toString();
+			}
+
+			public void modify(Object element, String property, Object value) {
+
+			}
+
+		});
+
+		v.setColumnProperties(new String[] { "1", "2" });
+		// v.setColumnProperties(new String[]
+		// {"Группа","Время","Дисциплина","Вид занятий","Преподователь","Кафедра"});
+
+		ColumnViewerEditorActivationStrategy actSupport = new ColumnViewerEditorActivationStrategy(
+				v) {
+			protected boolean isEditorActivationEvent(
+					ColumnViewerEditorActivationEvent event) {
+				return event.eventType == ColumnViewerEditorActivationEvent.TRAVERSAL
+						|| event.eventType == ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION
+						|| (event.eventType == ColumnViewerEditorActivationEvent.KEY_PRESSED && event.keyCode == SWT.CR);
+			}
+		};
+
+		GridViewerEditor.create(v, actSupport,
+				ColumnViewerEditor.TABBING_HORIZONTAL
+						| ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR
+						| ColumnViewerEditor.TABBING_VERTICAL
+						| ColumnViewerEditor.KEYBOARD_ACTIVATION);
+
+		final GridColumn groupColumn = new GridColumn(v.getGrid(), SWT.NONE);
+		groupColumn.setWidth(100);
+		groupColumn.setText("Группа");
+		groupColumn.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				super.widgetSelected(e);
+				ExcelFilter filter = new ExcelFilter(groupColumn);
+			}
 			
-	v.setCellEditors(new CellEditor[] { new TextCellEditor(v.getGrid()), new TextCellEditor(v.getGrid()) });
-	v.setCellModifier(new ICellModifier() {
+		});
+		
 
-		public boolean canModify(Object element, String property) {
-			return true;
-		}
+		final GridColumn dayColumn = new GridColumn(v.getGrid(), SWT.NONE);
+		dayColumn.setWidth(100);
+		dayColumn.setText("День");
+		dayColumn.addSelectionListener(new SelectionAdapter() {
 
-		public Object getValue(Object element, String property) {
-			return "Column " + property + " => " + element.toString();
-		}
-
-		public void modify(Object element, String property, Object value) {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				super.widgetSelected(e);
+				ExcelFilter filter = new ExcelFilter(dayColumn);
+			}
 			
-		}
+		});
 		
-	});
-	
-	v.setColumnProperties(new String[] {"1","2"});
-//	v.setColumnProperties(new String[] {"Группа","Время","Дисциплина","Вид занятий","Преподователь","Кафедра"});
-	
-	ColumnViewerEditorActivationStrategy actSupport = new ColumnViewerEditorActivationStrategy(v) {
-		protected boolean isEditorActivationEvent(ColumnViewerEditorActivationEvent event) {
-			return event.eventType == ColumnViewerEditorActivationEvent.TRAVERSAL
-					|| event.eventType == ColumnViewerEditorActivationEvent.MOUSE_DOUBLE_CLICK_SELECTION
-					|| (event.eventType == ColumnViewerEditorActivationEvent.KEY_PRESSED && event.keyCode == SWT.CR);
-		}
-	};
-	
-	GridViewerEditor.create(v, actSupport, ColumnViewerEditor.TABBING_HORIZONTAL
-			| ColumnViewerEditor.TABBING_MOVE_TO_ROW_NEIGHBOR
-			| ColumnViewerEditor.TABBING_VERTICAL | ColumnViewerEditor.KEYBOARD_ACTIVATION);
-	
-	GridColumn column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Группа");
-	final GridColumn tempColumn = column;
-	column.addSelectionListener(new SelectionListener() {
-		
-		@Override
-		public void widgetSelected(SelectionEvent e) {
-			// TODO Auto-generated method stub
-			System.out.println(e);
-			ExcelFilter filter = new ExcelFilter(tempColumn.getParent(), 
-					SWT.BORDER | SWT.DROP_DOWN);
-		}
-		
-		@Override
-		public void widgetDefaultSelected(SelectionEvent e) {
-			// TODO Auto-generated method stub
+		final GridColumn timeColumn = new GridColumn(v.getGrid(), SWT.NONE);
+		timeColumn.setWidth(100);
+		timeColumn.setText("Время");
+		timeColumn.addSelectionListener(new SelectionAdapter() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				super.widgetSelected(e);
+				ExcelFilter filter = new ExcelFilter(timeColumn);
+			}
 			
-		}
-	});
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("День");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Время");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Дата");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Дисциплина");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Вид занятий");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Аудитория");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Здание");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Должность");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Преподователь");
-	
-	column = new GridColumn(v.getGrid(), SWT.NONE);
-	column.setWidth(100);
-	column.setText("Кафедра");
-	
-//	v.setInput(getInput());
-	v.getGrid().setLinesVisible(true);
-	v.getGrid().setHeaderVisible(true);
-	v.getGrid().setRowHeaderVisible(true);
-	
-	listeners();
-    }
-    
-    protected abstract void listeners();
+		});
+		
+		GridColumn column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Дата");
 
-    protected abstract IBaseLabelProvider getLabelProvider();
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Дисциплина");
 
-    protected abstract IContentProvider getContentProvider();
-    
-    @Override
-	public void setInput(List<ScheduleEntry> elements) {
-    	v.setInput(elements);
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Вид занятий");
+
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Аудитория");
+
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Здание");
+
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Должность");
+
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Преподователь");
+
+		column = new GridColumn(v.getGrid(), SWT.NONE);
+		column.setWidth(100);
+		column.setText("Кафедра");
+
+		// v.setInput(getInput());
+		v.getGrid().setLinesVisible(true);
+		v.getGrid().setHeaderVisible(true);
+		v.getGrid().setRowHeaderVisible(true);
+
+		listeners();
 	}
-    
-    public void setFocus() {
-	composite.setFocus();
-    }
 
-    public void dispose() {
-	composite.dispose();
-    }
-    
-    private void createDropDownWindow() {
-    	
-    }
+	protected abstract void listeners();
+
+	protected abstract IBaseLabelProvider getLabelProvider();
+
+	protected abstract IContentProvider getContentProvider();
+
+	@Override
+	public void setInput(List<ScheduleEntry> elements) {
+		v.setInput(elements);
+	}
+
+	public void setFocus() {
+		composite.setFocus();
+	}
+
+	public void dispose() {
+		composite.dispose();
+	}
+
+	private void createDropDownWindow() {
+
+	}
 }
--- ru.kai.assistschedule.ui/src/main/java/ru/kai/assistschedule/ui/internal/widgets/ExcelFilter.java
+++ ru.kai.assistschedule.ui/src/main/java/ru/kai/assistschedule/ui/internal/widgets/ExcelFilter.java
@@ -1,47 +1,118 @@
 package ru.kai.assistschedule.ui.internal.widgets;
 
-import org.eclipse.nebula.cwt.base.BaseCombo;
-import org.eclipse.nebula.cwt.v.VCanvas;
-import org.eclipse.nebula.cwt.v.VGridLayout;
-import org.eclipse.nebula.cwt.v.VPanel;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.nebula.widgets.grid.GridColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.ShellListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
-public class ExcelFilter extends BaseCombo {
-
-	VPanel filterPanel;
+public class ExcelFilter {
 	
-	public ExcelFilter(Composite parent, int style) {
-		super(parent, style);
-		setOpen(false);
-	}
+	private GridColumn column;
 
-	@Override
-	protected boolean setContentFocus() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	private Shell columnShell;
+	
+	public ExcelFilter(GridColumn column) {
+		super();
+		this.column = column;
+		
+		final GridColumn dayColumn = column;
+		Shell mainShell = dayColumn.getParent().getShell();
+		// GridColumn column = dayColumn;
+		// column.getHeaderRenderer()
+		final Shell child = new Shell(mainShell, SWT.DOUBLE_BUFFERED);
+		child.setLocation(
+				dayColumn.getParent().toDisplay(0, 0).x
+						+ dayColumn.getHeaderRenderer().getBounds().x
+						+ dayColumn.getHeaderRenderer().getBounds().width
+						- 150,
+				dayColumn.getParent().toDisplay(0, 0).y
+						+ dayColumn.getHeaderRenderer().getBounds().height);
 
-	private void createFilter() {
-		Shell shell = getContentShell();
-		int style = SWT.BORDER | SWT.DOUBLE_BUFFERED;
-		VCanvas canvas = new VCanvas(shell, style);
-	    filterPanel = canvas.getPanel();
-	    filterPanel.setWidget(canvas);
-	    VGridLayout layout = new VGridLayout();
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		layout.verticalSpacing = 1;
-		filterPanel.setLayout(layout);
-		setContent(filterPanel.getComposite());
-	}
+		child.setLayout(new GridLayout());
+		final Composite composite = new Composite(child, SWT.NONE);
+		composite.setLayoutData(new GridData(GridData.FILL,
+				GridData.FILL, true, true));
+		composite.setLayout(new GridLayout());
+		Label label = new Label(composite, SWT.None);
+		label.setText("Hello Child!");
+		label.setLayoutData(new GridData(GridData.FILL, GridData.FILL,
+				true, true));
+		child.open();
+		
+		dayColumn.getDisplay().asyncExec(new Runnable() {
 
-	@Override
-	protected void setOpen(boolean open) {
-		// TODO Auto-generated method stub
-		super.setOpen(open);
-		createFilter();
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				int i = 1;
+				while (i++ < 11) {
+					child.setSize(150, i * 25);
+					try {
+						TimeUnit.MILLISECONDS.sleep(10);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		child.addShellListener(new ShellListener() {
+			
+			@Override
+			public void shellIconified(ShellEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(e);
+			}
+			
+			@Override
+			public void shellDeiconified(ShellEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(e);
+			}
+			
+			@Override
+			public void shellDeactivated(ShellEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("shellDeactivated");
+				dayColumn.getDisplay().asyncExec(new Runnable() {
+
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						int i = 10;
+						while (i-- > 0) {
+							child.setSize(150, i * 25);
+							try {
+								TimeUnit.MILLISECONDS.sleep(10);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						child.close();
+					}
+				});
+			}
+			
+			@Override
+			public void shellClosed(ShellEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(e);
+			}
+			
+			@Override
+			public void shellActivated(ShellEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println(e);
+			}
+		});
 	}
 	
 	
